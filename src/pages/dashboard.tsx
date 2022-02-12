ea2ed8eced18a07f17875d54de7206fa43fdba10
Scyu_ <scyu@scyu.dev>
2022-02-12 00:04:29 +0000

    updates

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,12 @@
 import AppLayout from '../components/Layouts/AppLayout'
 import Head from 'next/head'
+import { useAuth } from '../hooks/auth'
 
 const Dashboard = () => {
+    const { user } = useAuth({
+        middleware: 'auth',
+    })
+
     return (
         <AppLayout
             header={
@@ -17,7 +22,7 @@ const Dashboard = () => {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
-                            You're logged in!
+                            You're logged in as {user.username}!
                         </div>
                     </div>
                 </div>
